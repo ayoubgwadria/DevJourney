1cfee70aa02d228a6f9ead6ffc49ce6322928a7d
ayoubgwadria <ayoubgwadria@gmail.com>
2025-01-12 02:32:43 +0100

    home page beta version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/home/hero/hero.component';
+import { FeaturesComponent } from './components/home/features/features.component';
+import { HowItWorksComponent } from './components/home/how-it-works/how-it-works.component';
+import { TestimonialsComponent } from './components/home/testimonials/testimonials.component';
+import { CtaComponent } from './components/home/cta/cta.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    HeroComponent,
+    FeaturesComponent,
+    HowItWorksComponent,
+    TestimonialsComponent,
+    CtaComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
